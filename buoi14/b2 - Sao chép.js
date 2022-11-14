906225483424ef333eca8b427b3075220f2fd210
khanhnam28 <114087545+khanhnam28@users.noreply.github.com>
2022-11-14 11:50:58 +0700

    Delete b2 - Sao chép.js

--- buoi14/b2 - Sao chép.js
+++
@@ -1,9 +0,0 @@
-const numberOneTriangle = ( n ) => {
-    for(let i = 0; i< n; i++){
-      for(let j =0; j<i; j++) {
-        document.write("*")
-      }
-      document.write("<br/>")
-    }
-  }
-  numberOneTriangle(5);
